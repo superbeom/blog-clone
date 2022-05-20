c4cb0b4d423c28c63fc80ca3daef5c364c5e11f5
superbeom <it.super.beom@gmail.com>
2022-05-20 11:04:05 +0900

    Display Posts

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 import Head from 'next/head'
 
+import { getPosts } from '../services'
+
 import { PostCard, Categories, PostWidget } from '../components'
 
-const posts = [
-  { title: 'React Testing 1', excerpt: 'Learn React Testing 1' },
-  { title: 'React Testing 2', excerpt: 'Learn React Testing 2' },
-  { title: 'React Testing 3', excerpt: 'Learn React Testing 3' },
-  { title: 'React Testing 4', excerpt: 'Learn React Testing 4' },
-  { title: 'React Testing 5', excerpt: 'Learn React Testing 5' },
-]
-
-const Home = () => {
+const Home = ({ postDatas }) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -20,8 +14,8 @@ const Home = () => {
 
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => (
-            <PostCard key={post.title} post={post} />
+          {postDatas.map((postData) => (
+            <PostCard key={postData.cursor} post={postData.node} />
           ))}
         </div>
 
@@ -37,3 +31,13 @@ const Home = () => {
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const postDatas = (await getPosts()) || []
+
+  return {
+    props: {
+      postDatas,
+    },
+  }
+}
