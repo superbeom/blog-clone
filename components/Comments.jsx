986fb57ac5df551a4d5d6ee7bc362b0a5017385c
superbeom <it.super.beom@gmail.com>
2022-05-23 15:04:18 +0900

    Display comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+
+import { getComments } from '../services'
 
 const Comments = ({ slug }) => {
-  return <div>Comments</div>
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result))
+  }, [])
+
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+            {comments.length === 1
+              ? '1 Comment'
+              : `${comments.length} Comments`}
+          </h3>
+
+          {comments.map((comment) => (
+            <div
+              key={comment.id}
+              className="mb-4 border-b border-gray-100 pb-4"
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+              </p>
+
+              <p className="w-full whitespace-pre-line text-gray-600">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Comments
--- services/index.js
+++ services/index.js
@@ -144,3 +144,20 @@ export const submitComment = async (obj) => {
 
   return result.json()
 }
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        id
+        name
+        comment
+        createdAt
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.comments
+}
